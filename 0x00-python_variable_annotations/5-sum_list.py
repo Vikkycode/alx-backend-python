ac6a60f4e737f2dcefc6dab04a357436b64848ba
Victor <vicky.robilson55@gmail.com>
2024-10-10 21:22:31 -0700

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ sum function """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ write annotated function"""
     return sum(input_list)
