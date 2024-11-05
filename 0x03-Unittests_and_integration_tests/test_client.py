b163848e28a02885d6db8c3ec13c0ed89b249c59
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:50:49 +0100

    test has advanced

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,3 +42,28 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Test that has_license returns the correct value."""
         self.assertEqual(GithubOrgClient.has_license(repo, license_key),
                          expected)
+
+    def test_public_repos(self):
+        """ Test public repos method """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = (
+                {"repos_url": "https://api.github.com/orgs/google/repos"}
+            )
+            client = GithubOrgClient("google")
+            self.assertEqual(client.public_repos(),
+                             ['episodes.dart', 'cpp-netlib', 'dagger',
+                              'ios-webkit-debug-proxy', 'google.github.io',
+                              'kratu', 'build-debian-cloud',
+                              'traceur-compiler'])
+
+    def test_public_repos_with_license(self):
+        """ Test public repos with license argument """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = (
+                {"repos_url": "https://api.github.com/orgs/google/repos"}
+            )
+            client = GithubOrgClient("google")
+            self.assertEqual(client.public_repos(license="apache-2.0"),
+                             ['dagger', 'kratu'])
