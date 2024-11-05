4e068b8cd46516056a917aa0c82624318f352f8c
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:34:04 +0100

    test memorize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-"""Unittest for get_json
-"""
+"""test utils module."""
 
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized
 
-from utils import get_json
+from utils import memoize
 
 
-class TestGetJson(unittest.TestCase):
-    """Test class for the get_json utility function."""
+class TestMemoize(unittest.TestCase):
+    """Test class for the memoize decorator."""
 
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    @patch('requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """Test that utils.get_json returns the expected result."""
-        mock_get.return_value.json.return_value = test_payload
-        self.assertEqual(get_json(test_url), test_payload)
-        mock_get.assert_called_once_with(test_url)
+    def test_memoize(self):
+        """Test that memoize decorator caches the result of a function."""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch('test_utils.TestMemoize.test_memoize.'
+                   'TestClass.a_method') as mock_a_method:
+            # Create an instance of the class
+            instance = TestClass()
+            # Call a_property twice
+            self.assertEqual(instance.a_property, 42)
+            self.assertEqual(instance.a_property, 42)
+            # Assert that a_method was called only once
+            mock_a_method.assert_called_once()
