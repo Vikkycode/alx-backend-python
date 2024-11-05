4f2b7309410d12871d945d24651637a99d607f6d
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 06:01:24 +0100

    test method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,31 @@
 """test utils module."""
 
 
-def a_method(self):
-    return 42
+import unittest
+from unittest.mock import patch
 
-@memoize
-def a_property(self):
-    return self.a_method()
+from utils import memoize
+
+class TestMemoize(unittest.TestCase):
+    """Test class for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test that memoize decorator caches the result of a function."""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch('test_utils.TestMemoize.test_memoize.'
+                   'TestClass.a_method') as mock_a_method:
+            # Create an instance of the class
+            instance = TestClass()
+            # Call a_property twice
+            self.assertEqual(instance.a_property, 42)
+            self.assertEqual(instance.a_property, 42)
+            # Assert that a_method was called only once
+            mock_a_method.assert_called_once()
