2c39b25023d9a7d55fb08657f6726ec12ce1ad8b
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:57:54 +0100

    test get json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,48 +1,24 @@
 #!/usr/bin/env python3
 """test utils module."""
 
-# import unittest
-# from unittest.mock import patch
-
-# from utils import memoize
-
-
 import unittest
 from typing import Mapping, Any, Tuple
+import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test class for the access_nested_map utility function."""
+from utils import get_json
+
+class TestGetJson(unittest.TestCase):
+    """Test class for the get_json utility function."""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Tuple, expected: Any) -> None:
-        """Test that the function returns the expected value for valid inputs."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-# class TestMemoize(unittest.TestCase):
-#     """Test class for the memoize decorator."""
-
-#     def test_memoize(self):
-#         """Test that memoize decorator caches the result of a function."""
-
-#         class TestClass:
-#             def a_method(self):
-#                 return 42
-
-#             @memoize
-#             def a_property(self):
-#                 return self.a_method()
-
-#         with patch('test_utils.TestMemoize.test_memoize.'
-#                    'TestClass.a_method') as mock_a_method:
-#             # Create an instance of the class
-#             instance = TestClass()
-#             # Call a_property twice
-#             self.assertEqual(instance.a_property, 42)
-#             self.assertEqual(instance.a_property, 42)
-#             # Assert that a_method was called only once
-#             mock_a_method.assert_called_once()
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that utils.get_json returns the expected result."""
+        mock_get.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
