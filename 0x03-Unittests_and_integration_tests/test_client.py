7c18f533fd0df38ca2dd6bbaf76b6563363f31bf
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:40:02 +0100

    test org

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,3 +22,11 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f'https://api.github.com/orgs/{org_name}'
         )
+
+    def test_public_repos_url(self):
+        """Test that _public_repos_url returns the correct value."""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": "test_url"}
+            client = GithubOrgClient("test_org")
+            self.assertEqual(client._public_repos_url, "test_url")
