47b58d3686ade8007d68452b69685da711aac634
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:41:53 +0100

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,7 @@ import unittest
 from parameterized import parameterized
 from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -30,3 +31,19 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = {"repos_url": "test_url"}
             client = GithubOrgClient("test_org")
             self.assertEqual(client._public_repos_url, "test_url")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test that public_repos returns the expected list of repositories."""
+        mock_get_json.return_value = TEST_PAYLOAD[1]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = TEST_PAYLOAD[0]['repos_url']
+            client = GithubOrgClient("google")
+            self.assertEqual(client.public_repos(),
+                             ['episodes.dart', 'cpp-netlib', 'dagger',
+                              'ios-webkit-debug-proxy', 'google.github.io',
+                              'kratu', 'build-debian-cloud',
+                              'traceur-compiler'])
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
