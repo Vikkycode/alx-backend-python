eb55799d61e191f2e416fb74d757e8c15c3227a9
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:38:03 +0100

    test github org client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" tset gibuorgclient module"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, PropertyMock
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test class for GithubOrgClient."""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test that GithubOrgClient.org returns the correct value."""
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, mock_get_json.return_value)
+        mock_get_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org_name}'
+        )
