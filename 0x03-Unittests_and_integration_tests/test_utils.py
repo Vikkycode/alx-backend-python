44ddd8d29179ec080427f8e076fff8cc2506c811
victor oricha <vicky.robilson5528@gmail.com>
2024-11-05 05:17:56 +0100

    test untils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+import unittest
+from typing import Mapping, Any, Tuple
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test class for the access_nested_map utility function."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Tuple, expected: Any) -> None:
+        """Test that the function returns"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
